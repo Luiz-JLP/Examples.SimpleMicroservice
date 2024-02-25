bffc6568fd90d997bb5b45aca8d146c70e9bb3b9
Luiz-JLP <80367069+Luiz-JLP@users.noreply.github.com>
2024-02-25 16:53:27 -0300

    Criação da classe de domínio

---
+++ Services.People/src/Services.People.Domains/Interfaces/IPeopleDomain.cs
@@ -0,0 +1,48 @@
+﻿using Services.People.Models;
+
+namespace Services.People.Domains.Interfaces
+{
+    public interface IPeopleDomain
+    {
+        /// <summary>
+        /// Create a Person
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        Person Create(Person person);
+
+        /// <summary>
+        /// Create Many People
+        /// </summary>
+        /// <param name="people"></param>
+        /// <returns></returns>
+        IEnumerable<Person> Create(IEnumerable<Person> people);
+
+        /// <summary>
+        /// Read a Person by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Person Read(string id);
+
+        /// <summary>
+        /// Read All People
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Person> Read();
+
+        /// <summary>
+        /// Update a Person
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        Person Update(Person person);
+
+        /// <summary>
+        /// Delete a Person
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        bool Delete(Person person);
+    }
+}
---
+++ Services.People/src/Services.People.Domains/PeopleDomain.cs
@@ -0,0 +1,38 @@
+﻿using Services.People.Domains.Interfaces;
+using Services.People.Models;
+
+namespace Services.People.Domains
+{
+    public class PeopleDomain : IPeopleDomain
+    {
+        public Person Create(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> Create(IEnumerable<Person> people)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Read(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
