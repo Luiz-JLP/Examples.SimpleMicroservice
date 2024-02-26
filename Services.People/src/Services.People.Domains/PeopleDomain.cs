ecd918d6a441911ac86fbad8365922545375f63a
Luiz-JLP <80367069+Luiz-JLP@users.noreply.github.com>
2024-02-25 21:08:10 -0300

    Ajustes no Domain

--- Services.People/src/Services.People.Domains/PeopleDomain.cs
+++ Services.People/src/Services.People.Domains/PeopleDomain.cs
@@ -15,27 +15,53 @@ namespace Services.People.Domains
 
         public Person Create(Person person)
         {
+            if (String.IsNullOrEmpty(person.FirstName))
+                throw new ArgumentException("O Campo First Name é Obrigatório.");
+
+            if (String.IsNullOrEmpty(person.LastName))
+                throw new ArgumentException("O Campo Last Name é Obrigatório.");
+
+            person.Id = Guid.NewGuid().ToString();
+            person.CreationDate = DateTime.Now;
+            person.LastModified = DateTime.Now;
+
             return _context.Create(person);
         }
 
         public bool Delete(Person person)
         {
-            throw new NotImplementedException();
+            return _context.Delete(person);
         }
 
         public Person Read(string id)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrEmpty(id)) 
+                throw new ArgumentException("O Parâmetro ID é obrigatório.");
+
+            var person = _context.Read(id);
+
+            if (String.IsNullOrEmpty(person.Id))
+                throw new ArgumentOutOfRangeException("id", "Nenhum Registro encontrado para o ID passado.");
+
+            return person;
         }
 
         public IEnumerable<Person> Read()
         {
-            throw new NotImplementedException();
+            return _context.Read();
         }
 
         public Person Update(Person person)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrEmpty(person.FirstName))
+                throw new ArgumentException("O Campo First Name é Obrigatório.");
+
+            if (String.IsNullOrEmpty(person.LastName))
+                throw new ArgumentException("O Campo Last Name é Obrigatório.");
+
+            person.LastModified = DateTime.Now;
+
+            return _context.Update(person);
         }
     }
 }
